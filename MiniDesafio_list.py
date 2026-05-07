6563d9f2d4671c10aa3ebe58e3bdb7c4414fb25c
Rikk <tarikmoussa90@gmail.com>
2026-05-07 19:46:33 -0300

    feat: project/rev

--- MiniDesafio_list.py
+++ MiniDesafio_list.py
@@ -1,33 +1,32 @@
-#Nome do usuário
+#nome do usuário
 nome = input("Digite seu nome: ")
 
-#Lista para guardar as pontuações
+#lista para guardar as pontuações
 pontuacoes = []
 
-#Leitura das 6 pontuações
+#leitura das 6 pontuações
 for i in range(6):
-    ponto = float(input(f"Digite a pontuação da ação {i + 1}: "))
+    ponto = float(input(f"Digite o valor da ação {i + 1}: "))
     pontuacoes.append(ponto)
-#Total das pontuações
+#total das pontuações
 total = sum(pontuacoes)
 
-#Média
+#média
 media = total / len(pontuacoes)
 
-#Contador de ações com pontuação >= 80
+#contador de ações com pontuação >= 80
 quantidade_destaques = 0
-
 for valor in pontuacoes:
     if valor >= 80:
         quantidade_destaques += 1
 
-#Resultados
-print("\n--- RESULTADO ---")
+#resultados
+print("\n----------------------- RESULTADO --------------------")
 print(f"Usuário: {nome}")
 print(f"Pontuações: {pontuacoes}")
 print(f"Total: {total}")
 print(f"Média: {media:.2f}")
 print(f"Ações com pontuação maior ou igual a 80: {quantidade_destaques}")
 
-#Mensagem final personalizada
+#mensagem final personalizada
 print(f"\nParabéns, {nome}! Continue praticando ações sustentáveis!")
